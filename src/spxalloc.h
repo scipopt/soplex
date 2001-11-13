e9b1bdb772db93e385aa1347af66f7e38e54f594
Thorsten Koch <koch@zib.de>
2001-11-13 16:17:52 +0000

    Neue Speicherallozierungs Templates als ersatz für malloc()/Malloc()

---
+++ src/spxalloc.h
@@ -0,0 +1,85 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the class library                   */
+/*       SoPlex --- the Sequential object-oriented simPlex.                  */
+/*                                                                           */
+/*    Copyright (C) 1997-1999 Roland Wunderling                              */
+/*                  1997-2001 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SoPlex is distributed under the terms of the ZIB Academic Licence.       */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SoPlex; see the file COPYING. If not email to soplex@zib.de.  */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma ident "@(#) $Id: spxalloc.h,v 1.1 2001/11/13 16:17:52 bzfkocht Exp $"
+
+#ifndef _SPXALLOC_H_
+#define _SPXALLOC_H_
+
+#include <iostream>
+#include <stdlib.h>
+#include <assert.h>
+
+namespace soplex
+{
+///
+template <class T>
+void spx_alloc(T& p, int n)
+{
+   assert(n >= 0);
+
+   if (n == 0)
+      n = 1;
+   
+   p = reinterpret_cast<T>(malloc(sizeof(*p) * n));
+
+   if (0 == p)
+   {
+      std::cerr << "malloc: Out of memory" << std::endl;
+      abort();
+   }
+}
+
+/// 
+template <class T>
+void spx_realloc(T& p, int n)
+{
+   assert(p != 0);
+   assert(n >= 0);
+
+   p = reinterpret_cast<T>(realloc(p, sizeof(*p) * n));
+
+   if (0 == p)
+   {
+      std::cerr << "realloc: Out of memory" << std::endl;
+      abort();
+   }
+}
+
+///
+template <class T>
+void spx_free(T& p)
+{
+   assert(p != 0);
+
+   free(p);
+   
+   p = 0;
+}
+
+} // namespace soplex
+#endif // _SPXALLOC_H_
+
+//-----------------------------------------------------------------------------
+//Emacs Local Variables:
+//Emacs mode:c++
+//Emacs c-basic-offset:3
+//Emacs tab-width:8
+//Emacs indent-tabs-mode:nil
+//Emacs End:
+//-----------------------------------------------------------------------------
+
+
+
