b354cc3f271b39672d5dce32afc7a808679e88e4
Thorsten Koch <koch@zib.de>
2001-12-25 16:11:38 +0000

    Ersatz für inconsistent.

---
+++ src/spxmessage.cpp
@@ -0,0 +1,44 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the class library                   */
+/*       SoPlex --- the Sequential object-oriented simPlex.                  */
+/*                                                                           */
+/*    Copyright (C) 1997-1999 Roland Wunderling                              */
+/*                  1997-2001 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SoPlex is distributed under the terms of the ZIB Academic Licence.       */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SoPlex; see the file COPYING. If not email to soplex@zib.de.  */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma ident "@(#) $Id: spxmessage.cpp,v 1.1 2001/12/25 16:11:38 bzfkocht Exp $"
+
+#include <iostream>
+#include <assert.h>
+
+namespace soplex
+{
+
+int spxinconsistent(const char* name, const char* file, int line)
+{
+   assert(name != 0);
+   assert(file != 0);
+   assert(line >= 0);
+
+   std::cout << file << "(" << line << ") "
+             << "Inconsistency detected in" << name << std::endl; 
+
+   return 0;
+}
+} // namespace soplex
+
+//-----------------------------------------------------------------------------
+//Emacs Local Variables:
+//Emacs mode:c++
+//Emacs c-basic-offset:3
+//Emacs tab-width:8
+//Emacs indent-tabs-mode:nil
+//Emacs End:
+//-----------------------------------------------------------------------------
---
+++ src/spxmessage.h
@@ -0,0 +1,34 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the class library                   */
+/*       SoPlex --- the Sequential object-oriented simPlex.                  */
+/*                                                                           */
+/*    Copyright (C) 1997-1999 Roland Wunderling                              */
+/*                  1997-2001 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SoPlex is distributed under the terms of the ZIB Academic Licence.       */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SoPlex; see the file COPYING. If not email to soplex@zib.de.  */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma ident "@(#) $Id: spxmessage.h,v 1.1 2001/12/25 16:11:38 bzfkocht Exp $"
+
+namespace soplex
+{
+
+extern int spxinconsistent(const char* name, const char* file, int line);
+
+#define SPXinconsistent(name) spxinconsistent(name, __FILE__, __LINE__)
+ 
+} // namespace soplex
+
+//-----------------------------------------------------------------------------
+//Emacs Local Variables:
+//Emacs mode:c++
+//Emacs c-basic-offset:3
+//Emacs tab-width:8
+//Emacs indent-tabs-mode:nil
+//Emacs End:
+//-----------------------------------------------------------------------------
