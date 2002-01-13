9cc4c3a26f0ed9c26e19c49895433ebff5606614
Thorsten Koch <koch@zib.de>
2002-01-13 13:39:52 +0000

    Noch n bischen PATH

--- runtest.sh
+++ runtest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: runtest.sh,v 1.5 2002/01/12 22:02:44 bzfkocht Exp $
+# $Id: runtest.sh,v 1.6 2002/01/13 13:39:52 bzfkocht Exp $
 ARCH=`uname -m | sed -e s/sun../sparc/ -e s/i.86/x86/ -e s/IP../mips/ -e s/9000..../hppa/`
 OSTYPE=`uname -s | tr A-Z a-z`
 case $OSTYPE in
@@ -24,6 +24,7 @@ linux)
    ;;
 osf1)
    PATH=$PATH:/client/bin
+   export PATH
    cd /optimi/kombadon/bzfkocht/soplex
    gmake COMP=compaq OPT=opt clean 
    gmake COMP=compaq OPT=opt
@@ -33,6 +34,8 @@ osf1)
    gmake COMP=gnu    OPT=opt check >/dev/null 2>&1 &
    ;;
 sunos)
+   PATH=$PATH:/client/bin
+   export PATH
    cd /optimi/kombadon/bzfkocht/soplex
    gmake COMP=sun    OPT=opt clean 
    gmake COMP=sun    OPT=opt
@@ -43,20 +46,16 @@ sunos)
    ;;
 hp-ux)   
    PATH=$PATH:/usr/local/bin:/client/bin
-   CVS_RSH=ssh
-   export CVS_RSH
+   export PATH 
    cd $HOME/soplex
-   cvs update
    gmake COMP=hp     OPT=opt clean 
    gmake COMP=hp     OPT=opt
    gmake COMP=hp     OPT=opt check >/dev/null 2>&1 &
    ;;
 irix)
    PATH=$PATH:/usr/local/bin:/client/bin
-   CVS_RSH=ssh
-   export CVS_RSH
+   export PATH 
    cd $HOME/soplex
-   cvs update
    gmake COMP=sgi    OPT=std clean 
    gmake COMP=sgi    OPT=std
    gmake COMP=sgi    OPT=std check >/dev/null 2>&1 &
