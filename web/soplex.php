9052389782ec4df9b385b2f7b718af642dfe379e
Thorsten Koch <koch@zib.de>
2002-01-16 15:38:46 +0000

    Noch eine Kleinigkeit.

--- web/soplex.php
+++ web/soplex.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
 "http://www.w3.org/TR/REC-html40/loose.dtd">
-<!-- $Id: soplex.php,v 1.5 2002/01/16 14:49:06 bzfkocht Exp $ -->
+<!-- $Id: soplex.php,v 1.6 2002/01/16 15:38:46 bzfkocht Exp $ -->
 <?php
 $counter = "cgi/counter.dat";  # the data storage file
 
@@ -23,7 +23,7 @@ fclose($fp);
 <meta name="author" content="Thorsten Koch">
 <meta name="description" content="SoPlex Homepage">
 <meta name="keywords" content="SoPlex, Simplex, Solver, LP, Linear Programming, Mathematical Programming, Optimization">
-<meta name="date" content="$Date: 2002/01/16 14:49:06 $">
+<meta name="date" content="$Date: 2002/01/16 15:38:46 $">
 <meta name="robots" content="index|follow">
 <meta http-equiv="content-type" content="text/html;charset=iso-8859-1">
 <title>SoPlex</title> 
@@ -126,7 +126,7 @@ LP files, already decompressed and assembled as an archive
 </p>
 <h3>Bugs</h3>
 <p>
-If you find one (shouldn't be to difficult), it would be nice if you 
+If you find one, it would be nice if you 
 send a description together with a data file that shows the
 problem or even better a working fix to 
 <a href="mailto://koch@zib.de">koch@zib.de</a>.
@@ -182,7 +182,7 @@ border="0" width="88" height="36" align="bottom"></a>
 <img src="images/valid-html40.png" alt="Valid HTML 4.0!"
 border="0" height="31" width="88" align="bottom"></a>
 <hr noshade><address><font size=-1>
-Last Update $Date: 2002/01/16 14:49:06 $ by
+Last Update $Date: 2002/01/16 15:38:46 $ by
 <a href="/personal/personal.pl?name=koch">Thorsten Koch</a>
 <br>&copy; 2002 by Konrad-Zuse-Zentrum für Informationstechnik Berlin (ZIB)<br>
 http://www.zib.de/Optimization/Software/Soplex/soplex.php
