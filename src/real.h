ce7cc31678c6d0aadeb767105eaf052a2bcb574a
Thorsten Koch <koch@zib.de>
2002-01-28 14:57:44 +0000

    Real erweitert.

--- src/real.h
+++ src/real.h
@@ -13,7 +13,7 @@
 /*  along with SoPlex; see the file COPYING. If not email to soplex@zib.de.  */
 /*                                                                           */
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-#pragma ident "@(#) $Id: real.h,v 1.3 2002/01/21 11:28:13 bzfkocht Exp $"
+#pragma ident "@(#) $Id: real.h,v 1.4 2002/01/28 14:57:44 bzfkocht Exp $"
 
 /**@file  real.h
  * @brief Floating point type definition.
@@ -21,30 +21,58 @@
 #ifndef _REAL_H_
 #define _REAL_H_
 
+#include <math.h>
+
 namespace soplex
 {
 #ifdef WITH_LONG_DOUBLE
 
 typedef long double Real;
 
-#define EPS_ZERO  1e-16  // additive zero. 1.0 + EPS_ZERO == 1.0
+#define DEFAULT_EPS_ZERO  1e-16  // additive zero. 1.0 + EPS_ZERO == 1.0
+#define DEFAULT_INFINITY  1e100
 
 #else
 
 typedef double Real;
 
-#define EPS_ZERO  1e-32  // additive zero. 1.0 + EPS_ZERO == 1.0
+#define DEFAULT_EPS_ZERO  1e-32  // additive zero. 1.0 + EPS_ZERO == 1.0
+#define DEFAULT_INFINITY  1e100
 
 #endif // !WITH_LONG_DOUBLE
 
-/* Hi Fortran !
- */
-#define EQ(a, b)   (fabs((a) - (b)) <= EPS_ZERO)
-#define NE(a, b)   (fabs((a) - (b)) >  EPS_ZERO)
-#define LT(a, b)   (((a) - (b))     < -EPS_ZERO)
-#define LE(a, b)   (((a) - (b))     <  EPS_ZERO)
-#define GT(a, b)   (((a) - (b))     >  EPS_ZERO)
-#define GE(a, b)   (((a) - (b))     > -EPS_ZERO)
+extern const Real Eps_zero;
+extern const Real Infinity;
+
+inline bool EQ(Real a, Real b, Real eps = Eps_zero)
+{
+   return fabs(a - b) <= eps;
+}
+
+inline bool NE(Real a, Real b, Real eps = Eps_zero)
+{
+   return fabs(a - b) > eps;
+}
+
+inline bool LT(Real a, Real b, Real eps = Eps_zero)
+{
+   return (a - b) < -eps;
+}
+
+inline bool LE(Real a, Real b, Real eps = Eps_zero)
+{
+   return (a - b) < eps;
+}
+
+inline bool GT(Real a, Real b, Real eps = Eps_zero)
+{
+   return (a - b) > eps;
+}
+
+inline bool GE(Real a, Real b, Real eps = Eps_zero)
+{
+   return (a - b) > -eps;
+}
 
 } // namespace soplex
 #endif // _REAL_H_
