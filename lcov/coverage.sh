81e7e33204c071d3eef535106c09a621c08bf524
Sebastion Orlowski <orlowski@zib.de>
2005-08-10 13:57:57 +0000

    Changed order of consistency checks: 'make check' also compiles a new version.

--- lcov/coverage.sh
+++ lcov/coverage.sh
@@ -56,20 +56,6 @@ then
   exit 1
 fi
 
-# Check existence of a .gcno file to ensure proper gcov (version >=3.4).
-if !(test -f $BASE_DIR/$OBJ_DIR/spxsolve.gcno)
-then
-  echo "object files in $OBJ_DIR seem not to be compiled with gcc >= 3.4 and gcov activated (no .gcno files)"
-  exit 1
-fi
-
-# We need to ensure that there is a link to "src" in $OBJ_DIR, otherwise
-# gcov and lcov will get confused.
-if !(test -L $BASE_DIR/$OBJ_DIR/src)
-then
-  ln --symbolic $BASE_DIR/$SRC_DIR $BASE_DIR/$OBJ_DIR/src
-fi
-
 # Reset counters, ie remove data files
 #
 # This command is supposed to do the job, but only removes .da files while
@@ -78,11 +64,26 @@ fi
 echo Removing .gcda files
 rm -f $OBJ_DIR/*.gcda
 
-# Create coverage data by running the test suite.
+# Compile soplex with LCOV support and create coverage data by running the test suite.
 cd ..
 make COMP=gnu OPT=gcov TEST=$TEST_SUITE ALGO="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15" check
 cd lcov
 
+# We need to ensure that there is a link to "src" in $OBJ_DIR, otherwise
+# gcov and lcov will get confused.
+if !(test -L $BASE_DIR/$OBJ_DIR/src)
+then
+  ln --symbolic $BASE_DIR/$SRC_DIR $BASE_DIR/$OBJ_DIR/src
+fi
+
+# Check existence of a .gcno file to ensure proper gcov (version >=3.4).
+if !(test -f $BASE_DIR/$OBJ_DIR/spxsolve.gcno)
+then
+  echo "object files in $OBJ_DIR seem not to be compiled with gcc >= 3.4 and gcov activated (no .gcno files)"
+  exit 1
+fi
+
 # Build HTML files.
 $LCOV --capture -directory $BASE_DIR/$OBJ_DIR --output-file $TEST_SUITE.info --test-name $TEST_SUITE
 $GENHTML $TEST_SUITE.info --output-directory $TEST_SUITE --title "$TEST_SUITE LP suite" --show-details 
+
