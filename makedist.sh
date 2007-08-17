277a4d09d393727f64f381ba1c6c3ffcfccbf3ac
Tobias Achterberg <achterberg@zib.de>
2007-08-17 10:12:18 +0000

    - updated makedist.sh to be similar to ZIMPL

--- makedist.sh
+++ makedist.sh
@@ -1,3 +1,31 @@
 #!/bin/sh
-rm -f soplex-1.3.1.tgz
-tar -cvzhf soplex-1.3.1.tgz --files-from=soplex-1.3.1/distrib.list --exclude="*CVS*" --exclude="*~" --exclude=".?*" --exclude="*exercise_LP_changes.cpp" --exclude="*/local/*" 
+VERSION="1.3.1"
+NAME="soplex-$VERSION"
+rm -f $NAME
+ln -s . $NAME
+rm -f $NAME.tgz
+tar -cvzhf $NAME.tgz \
+--exclude="*CVS*" \
+--exclude="*~" \
+--exclude=".?*" \
+--exclude="*exercise_LP_changes.cpp" \
+--exclude="*/local/*" \
+$NAME/COPYING \
+$NAME/README \
+$NAME/CHANGELOG \
+$NAME/Makefile \
+$NAME/check/netlib.test \
+$NAME/check/quick.test \
+$NAME/check/mittelmann.test \
+$NAME/check/infeas.test \
+$NAME/check/netlib.solu \
+$NAME/check/quick.solu \
+$NAME/check/mittelmann.solu \
+$NAME/check/infeas.solu \
+$NAME/check/check.sh \
+$NAME/check/check.awk \
+$NAME/doc/soplex.dxy \
+$NAME/doc/xternal.cpp \
+$NAME/make/* \
+$NAME/src/*
+rm -f $NAME
