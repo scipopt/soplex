f3ac88664b78ca430cccb9b4bd34009b26e973f2
Tobias Achterberg <achterberg@zib.de>
2007-08-17 10:02:43 +0000

    - remove old tgz before creating new one

--- distrib.sh
+++ distrib.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+rm -f soplex-1.3.1.tgz
 tar -cvzhf soplex-1.3.1.tgz --files-from=soplex-1.3.1/distrib.list --exclude="*CVS*" --exclude="*~" --exclude=".?*" --exclude="*exercise_LP_changes.cpp" --exclude="*/local/*" 
