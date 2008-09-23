5e39411131cb6387b867b8bdf39d2375b048aea2
Robert Waniek <waniek@zib.de>
2008-09-23 15:07:18 +0000

    *** empty log message ***

--- doc/xternal.cpp
+++ doc/xternal.cpp
@@ -1,6 +1,6 @@
 //-----------------------------------------------------------------------------
 /**@mainpage SoPlex
-   @version  1.3.3
+   @version  1.4.0
    @author   Roland Wunderling
    @author   Tobias Achterberg
    @author   Timo Berthold
