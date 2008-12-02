e20dbaa4b6e9a35961868ec4434eb2a9cfb759bf
Marc Pfetsch <m.pfetsch@tu-bs.de>
2008-12-02 18:16:10 +0000

    - adding targetChangeObjective

---
+++ tests/testChangeObjective.cpp
@@ -0,0 +1,52 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                                           */
+/*                  This file is part of the class library                   */
+/*       SoPlex --- the Sequential object-oriented simPlex.                  */
+/*                                                                           */
+/*    Copyright (C) 1997-1999 Roland Wunderling                              */
+/*                  1997-2008 Konrad-Zuse-Zentrum                            */
+/*                            fuer Informationstechnik Berlin                */
+/*                                                                           */
+/*  SoPlex is distributed under the terms of the ZIB Academic Licence.       */
+/*                                                                           */
+/*  You should have received a copy of the ZIB Academic License              */
+/*  along with SoPlex; see the file COPYING. If not email to soplex@zib.de.  */
+/*                                                                           */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#pragma ident "@(#) $Id: testChangeObjective.cpp,v 1.1 2008/12/02 18:16:10 bzfpfets Exp $"
+
+/* Test a for a bug in soplex (version 1.4), which avoids the change of the objective. */
+
+#include <sstream>
+#include <soplex.h>
+
+using namespace soplex;
+
+const char lpstr[] =
+  "Maximize               \n"
+  " obj: x0 + x1          \n"
+  "Subject To             \n"
+  " c1: x0 + x1 <= 1      \n"
+  " c2: x0 + x1 >= -1     \n"
+  " c3: x0 - x1 <= 1      \n"
+  " c4: x0 - x1 >= -1     \n"
+  "Bounds                 \n"
+  " 0 <= x0               \n"
+  " 0 <= x1               \n"
+  "End                    \n";
+
+int main() 
+{
+   SoPlex lp;
+   std::istringstream is(lpstr);
+   lp.readLPF(is);
+
+   lp.solve();
+   std::cerr << lp.objValue() << std::endl;
+
+   lp.changeSense(SoPlex::MINIMIZE);
+   lp.solve();
+   std::cerr << lp.objValue() << std::endl;
+
+   return 0;
+}
