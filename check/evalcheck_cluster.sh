e588c804585452e6418af4725a745c29973d03f7
Matthias Miltenberger <miltenberger@zib.de>
2013-09-11 17:13:41 +0200

    new testcluster target for cluster runs
    usage:
    make testcluster TIME=3600 EXCLUSIVE=false

---
+++ check/evalcheck_cluster.sh
@@ -0,0 +1,136 @@
+#!/usr/bin/env bash
+#* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *#
+#*                                                                           *#
+#*                  This file is part of the class library                   *#
+#*       SoPlex --- the Sequential object-oriented simPlex.                  *#
+#*                                                                           *#
+#*    Copyright (C) 1996-2012 Konrad-Zuse-Zentrum                            *#
+#*                            fuer Informationstechnik Berlin                *#
+#*                                                                           *#
+#*  SoPlex is distributed under the terms of the ZIB Academic Licence.       *#
+#*                                                                           *#
+#*  You should have received a copy of the ZIB Academic License              *#
+#*  along with SoPlex; see the file COPYING. If not email to soplex@zib.de.  *#
+#*                                                                           *#
+#* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *#
+
+export LANG=C
+
+REMOVE=0
+AWKARGS=""
+FILES=""
+
+for i in $@
+do
+  if test ! -e $i
+  then
+      if test "$i" = "-r"
+      then
+	  REMOVE=1
+      else
+	  AWKARGS="$AWKARGS $i"
+      fi
+  else
+      FILES="$FILES $i"
+  fi
+done
+
+for FILE in $FILES
+do
+
+  DIR=`dirname $FILE`
+  EVALFILE=`basename $FILE .eval`
+  EVALFILE=`basename $EVALFILE .out`
+
+  OUTFILE=$DIR/$EVALFILE.out
+  ERRFILE=$DIR/$EVALFILE.err
+  RESFILE=$DIR/$EVALFILE.res
+
+  # check if the eval file exists; if this is the case construct the overall solution files
+  if test -e $DIR/$EVALFILE.eval
+  then
+      # in case an output file exists, copy it away to save the results
+      DATEINT=`date +"%s"`
+      if test -e $OUTFILE
+      then
+	  cp $OUTFILE $OUTFILE.old-$DATEINT
+      fi
+      if test -e $ERRFILE
+      then
+	  cp $ERRFILE $ERRFILE.old-$DATEINT
+      fi
+
+      echo > $OUTFILE
+      echo > $ERRFILE
+      echo create overall output and error file for $EVALFILE
+
+      for i in `cat $DIR/$EVALFILE.eval` DONE
+	do
+	if test "$i" = "DONE"
+	then
+	    break
+	fi
+
+	FILE=$i.out
+	if test -e $FILE
+	then
+	    cat $FILE >> $OUTFILE
+	    if test "$REMOVE" = "1"
+	    then
+		rm -f $FILE
+	    fi
+	else
+	    echo Missing $i
+	fi
+
+	FILE=$i.err
+	if test -e $FILE
+	then
+	    cat $FILE >> $ERRFILE
+	    if test "$REMOVE" = "1"
+	    then
+		rm -f $FILE
+	    fi
+	fi
+
+      done
+
+      if test "$REMOVE" = "1"
+      then
+	  rm -f $DIR/$EVALFILE.eval
+      fi
+  fi
+
+  # check if the out file exists
+  if test -e $DIR/$EVALFILE.out
+  then
+      echo create results for $EVALFILE
+
+      # detect test set
+      TSTNAME=`echo $EVALFILE | sed 's/check.\([a-zA-Z0-9_-]*\).*/\1/g'`
+
+      echo "Testset " $TSTNAME
+
+      if test -f $TSTNAME.test
+      then
+	  TESTFILE=$TSTNAME.test
+      else
+	  TESTFILE=""
+      fi
+
+      if test -f $TSTNAME.solu
+      then
+	  SOLUFILE=$TSTNAME.solu
+      else
+	  if test -f all.solu
+	  then
+	      SOLUFILE=all.solu
+	  else
+	      SOLUFILE=""
+	  fi
+      fi
+
+      awk -f check.awk $AWKARGS $SOLUFILE $OUTFILE | tee $RESFILE
+
+  fi
+done
