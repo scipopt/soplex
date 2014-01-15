7a58da582d5f3636c3405fa4563d90c6db22d015
Matthias Miltenberger <miltenberger@zib.de>
2014-01-15 15:37:59 +0100

    corrections on evaluation and check scripts

--- check/evaluation.py
+++ check/evaluation.py
@@ -7,17 +7,29 @@ import math
 import pandas as pd
 from decimal import Decimal
 
+# this function checks for the type of parsed string
+def typeofvalue(text):
+    try:
+        int(text)
+        return int
+    except ValueError:
+        pass
+
+    try:
+        float(text)
+        return float
+    except ValueError:
+        pass
+
+    return str
 
 soluname = sys.argv[1]
 testname = sys.argv[2]
 outname = sys.argv[3]
 
-solufile = open(soluname,'r')
-testfile = open(testname,'r')
 outfile = open(outname,'r')
-
-solulines = solufile.readlines()
 outlines = outfile.readlines()
+outfile.close()
 
 # print identifier
 print
@@ -62,7 +74,7 @@ for idx, outline in enumerate(outlines):
         instances[instancename] = {key:val for key,val in data.iteritems()}
 
     # invalidate instancename
-    if outline.startswith('=ready='):
+    elif outline.startswith('=ready='):
         instancename = ''
 
     elif outline.startswith('SoPlex status'):
@@ -87,19 +99,37 @@ for idx, outline in enumerate(outlines):
     elif outline.startswith('Total time'):
         instances[instancename]['time'] = float(outline.split()[-1])
 
-    elif outline.startswith('Violations'):
-        instances[instancename]['primalviol'] = float(outlines[idx+2].split()[-3])
-        instances[instancename]['dualviol'] = float(outlines[idx+3].split()[-3])
+    elif outline.startswith('Violation'):
+        primviol = outlines[idx+2].split()[-3]
+        dualviol = outlines[idx+3].split()[-3]
+        if typeofvalue(primviol) in [int,float] and typeofvalue(dualviol) in [int,float]:
+            instances[instancename]['primalviol'] = float(primviol)
+            instances[instancename]['dualviol'] = float(dualviol)
+        else:
+            instances[instancename]['primalviol'] = '-'
+            instances[instancename]['dualviol'] = '-'
 
     elif outline.startswith('Primal solution infeasible') or outline.startswith('Dual solution infeasible'):
         instances[instancename]['status'] = 'fail'
         fail = fail + 1
 
 # parse data from solufile
-for soluline in solulines:
-    tag, name, value = soluline.split()
-    value = float(value)
-    if instances.has_key(name):
+solufile = open(soluname,'r')
+
+for soluline in solufile:
+    solu = soluline.split()
+    tag = solu[0]
+    name = solu[1]
+    if len(solu) == 3:
+        value = solu[2]
+        if typeofvalue(value) in [int,float]:
+            value = float(value)
+    else:
+        if tag == '=inf=':
+            value = 'infeasible'
+        else:
+            value = 'unknown'
+    if name in instances:
         instances[name]['soluval'] = value
         # check solution status
         if value in ['infeasible', 'unbounded']:
@@ -107,13 +137,36 @@ for soluline in solulines:
                 instances[name]['status'] = 'fail'
         else:
             if (abs(instances[name]['value'] - value))/max(abs(instances[name]['value']),abs(value)) > tolerance:
-                instances[name]['status'] = 'inconsistent'
+                instances[name]['status'] = '('+str(value)+') inconsistent'
+
+solufile.close()
 
 df = pd.DataFrame(instances).T
 
-fails = sum(1 for name in instances.keys() if instances[name]['status'] == 'fail')
-timeouts = sum(1 for name in instances.keys() if instances[name]['status'] == 'timeout')
+fails = sum(1 for name in instances if instances[name]['status'] == 'fail')
+timeouts = sum(1 for name in instances if instances[name]['status'] == 'timeout')
+infeasible = sum(1 for name in instances if instances[name]['status'] == 'infeasible')
+optimal = sum(1 for name in instances if instances[name]['status'] == 'optimal')
 
-print df[['rows','cols','primalviol','dualviol','value','iters','time','status']].to_string(float_format=lambda x:"%6.9g"%(x))
+print df[['rows','cols','primalviol','dualviol','iters','time','value','status']].to_string(float_format=lambda x:"%6.9g"%(x))
 print
-print 'total: {} fails: {} timeout: {}'.format(len(instances),fails,timeouts)
+print 'Results: (testset '+testname.split('/')[-1].split('.')[-2]+', settings '+outname.split('/')[-1].split('.')[-2]+'):'
+print '{} total, {} optimal, {} fails, {} timeouts, {} infeasible'.format(len(instances),optimal,fails,timeouts,infeasible)
+
+# check for missing files
+testfile = open(testname,'r')
+
+for testline in testfile:
+    linesplit = testline.split('/')
+    linesplit = linesplit[len(linesplit) - 1].rstrip(' \n').rstrip('.gz').rstrip('.GZ').rstrip('.z').rstrip('.Z')
+    linesplit = linesplit.split('.')
+    instancename = linesplit[0]
+    for i in range(1, len(linesplit)-1):
+        instancename = instancename + '.' + linesplit[i]
+    length = len(instancename)
+    if length > namelength:
+        instancename = instancename[length-namelength-2:length-2]
+    if not instancename in instances:
+        print 'mssing instance: '+instancename
+
+testfile.close()
