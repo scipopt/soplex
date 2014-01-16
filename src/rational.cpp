ed3fd72344658407ba9082a654b59738389e4915
Ambros Gleixner <gleixner@zib.de>
2014-01-16 14:23:40 +0100

    corrected sizeof(long double) usage in rational.cpp
    otherwise Visual Studio 2013 does not compile

--- src/rational.cpp
+++ src/rational.cpp
@@ -2363,7 +2363,7 @@ bool Rational::isAdjacentTo(const double& d)
 /// returns precision of Rational implementation, i.e., number of bits used to store Rational numbers (INT_MAX if exact)
 int Rational::precision()
 {
-   return sizeof(dpointer->privatevalue);
+   return sizeof(long double);
 }
 
 
