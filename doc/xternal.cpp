4f8f4d5e7ae9ded223dd21889e7e5ad41d67385c
Ambros Gleixner <gleixner@zib.de>
2014-02-21 16:36:33 +0100

    update main page of doc (xternal.cpp)

--- doc/xternal.cpp
+++ doc/xternal.cpp
@@ -31,15 +31,13 @@
    current ANSI standard. RTTI and STL (other then iostream) are not used. 
    Everything is in one namespace \em soplex.
 
-   - \ref RUN      "Where does it run"
-
    - \ref INST     "Installation"
-
-   - \ref FLAGS    "How to use the flags in the example program"
 
    - \ref FAQ      "Frequently asked questions"
 
-   - \ref IR       "Iterative Refinement"
+   - \ref SHELL    "How to use the SoPlex command line"
+
+   - \ref LIB      "How to use SoPlex as a callable library"
 
    - \ref PROG     "Programming with SoPlex"
 */
@@ -98,154 +96,39 @@
     DataArray or Array.  
 */
 //-----------------------------------------------------------------------------
-/**@page RUN On which Platforms is SoPlex running
-
-   The current release of SoPlex was tested to compile with the following
-   compilers / architectures:
-   <TABLE>
-   <TR><TD>Architecture</TD><TD>CPU              </TD><TD>OS                </TD><TD>Compiler             </TD></TR>
-   <TR><TD>x86         </TD><TD>Pentium 4 3.2 GHz</TD><TD>Suse Linux 10.0   </TD><TD>GCC 4.0.2            </TD></TR>
-   <TR><TD>x86         </TD><TD>Pentium 4 3.8 GHz</TD><TD>Suse Linux 10.0   </TD><TD>Intel 9.0.026        </TD></TR>
-   <TR><TD>x86         </TD><TD>Pentium 4 3.2 GHz</TD><TD>Windows XP SP2    </TD><TD>MS Visual Studio 2003</TD></TR>
-   <TR><TD>x86_64      </TD><TD>AMD Opteron 875  </TD><TD>SLES 9            </TD><TD>GCC 4.0.2            </TD></TR>
-   <TR><TD>PowerPC     </TD><TD>G5 2.3           </TD><TD>MacOS-X 10.4.4    </TD><TD>GCC 4.0.0            </TD></TR>
-   <TR><TD>Power4      </TD><TD>Power4 1.7 GHz   </TD><TD>AIX 5.1           </TD><TD>VisualAge 6          </TD></TR>
-   <TR><TD>Alpha       </TD><TD>21264a 750 MHz   </TD><TD>OSF1 V5.1         </TD><TD>Compaq 6.5-39        </TD></TR>
-   <TR><TD>Sparc       </TD><TD>Ultra 60         </TD><TD>Solaris 9         </TD><TD>Sun Studio 11        </TD></TR>
-   </TABLE>
-
-   We expect SoPlex to be compilable using any sufficiently recent compiler.
-   Remember, your mileage may vary.
- */
-//-----------------------------------------------------------------------------
 /**@page INST Installation
- 
- \section Prerequisites Prerequisites 
- You need the following programs to compile SoPlex:
 
-  - C++ compiler (e.g. http://www.gnu.org/software/gcc)
-  - gnu make http://www.gnu.org/software/make
-  - gnu awk http://www.gnu.org/software/gawk (if you want the \c check target)
-  - doxygen http://www.doxygen.org (if you want to generate the documentation)
-
- After receiving SoPlex you have to uncompress it with \c gzip 
- (http://www.gnu.org/software/gzip) and 
- unpack it with \c tar (http://www.gnu.org/software/tar)
- into a directory. Then change to that directory.
-
- \section Tested Linux/x86, Linux/AXP, Darwin, Tru64, Solaris, IRIX, HP-UX
- If you are working under one those OSes you should try the following:
- 
- \c gmake \c COMP=xxx \c OPT=yyy
-
- with \c xxx being one of \c gnu, \c intel, \c sun, \c hp, \c sgi, \c compaq
- or \c ibm
- and \c yyy one of \c dbg (if you want the debug version) or \c opt 
- (if you want the optimized version).
-
- This should generate a binary in the \c bin subdirectory. 
-
- \section Others If the previous section was not for you
- First the Makefile tries to find out the OS and the 
- architecture by using the \c uname command. 
- If your OS or architecture is missing, please update the 
- Makefile and submit the change to me.
-
- Then a submakefile from \c make/make.OS.ARCH.COMP.OPT is included in
- the main Makefile. You should adapt the compiler flags as needed.
- Be especially careful with the \c AR setting since some C++ compilers do
- not like using the standard \c \c ar program together with code that uses
- templates.
-
- If this all does not work, change to the \c src directory and type
-
- \c CC \c *.cpp 
-
- This should do the trick. Adding \c -DNDEBUG gives you a non debugging 
- version. Add flags as needed. 
-
- \section Testing Testing the Binary
- After you compiled the binary you should download the Netlib LP files at
- http://www.zib.de/Optimization/Software/Soplex/netlib.tar.gz and
- unpack them in the \c check directory. Then you can try
-
- \c gmake \c COMP=xxx \c OPT=dbg \c quick
-
- \c gmake \c COMP=xxx \c OPT=opt \c quick
-
- \c gmake \c COMP=xxx \c OPT=opt \c check
-
- Use the \c check target together with \c OPT=dbg only if you have really 
- a lot of time. \c quick should run in a few minutes and \c check will 
- need between less than one hour and a day, depending on your machine.
-
- \c quick should report no fails at all. \c check should report no fails in the
- \c LC and \c EC columns and in the \c LR and \c ER columns only with 
- the instances greenbea, greenbeb, pilot-ja or pilot87. 
- One or two fails is normal, above four is probably a problem with the 
- compiler. Look how big the error is. Try again with less optimization. 
- Our results are available at 
- http://www.zib.de/Optimization/Software/Soplex/results
- to compare with.
-
- \section Documentation  Generating the documentation
- If you have \c doxygen (and \c dot) installed, you just can say 
-
- \c gmake \c doc
-
- After that the documentation should be in doc/html.
-
- \section Installation Installation
- The binary is in the \c bin directory, the library in \c lib and all
- headers are in \c src. Feel free to install them at a suitable place.
-
- \section Naming Naming of the OPT Variable
-
-  - dbg (debugging) -DNDEBUG is \b not set. Optimization is mostly off
-    and debugging info is generated.
-
-  - std (standard) -DNDEBUG is set. All optimizations may be switched on
-    that do \b not \b alter the floating point behaviour or may 
-    otherwise by result in wrong code.
-
-  - opt (optimized) -DNDEBUG is set. All optimizations may be switched on,
-    as long as the code seems to run correctly. The code should run
-    on the relevant architectures. Best is something like -fast that
-    uses the right optimizations for the architecture that is used.
-
-  - opt-XXX (optimized for XXX) like opt-p4. This includes optimization
-    for a specific processor. 
-
-  - prf (profile) like opt, but generate profile data.
-
-  - XXX-ld (long-double) same as XXX but with long doubles.
+   See the INSTALL file in the distribution.
  */
 //-----------------------------------------------------------------------------
 /**@page FAQ Frequently Asked Questions
  * \htmlinclude faq_new.inc
  */           
 //-----------------------------------------------------------------------------
-/**@page FLAGS How to use the flags in the example program
+/**@page SHELL How to use the SoPlex command line
 
-   Here are some tips on which flags to use with the example program:
+   Running the command line version of SoPlex without any arguments displays
+   a list of options.  You can write a parameter file with default parameters
+   by using option --saveset=FILENAME.set.  After changing parameter values
+   in this file you can use it by with --loadset=FILENAME.set.
+   The most frequently used parameters have abbreviations as explained in the
+   initial help.
 
-   If you have more constraints (rows) than variables (cols) it is 
-   a good idea to try the \c -r flag to choose a row-wise representation
-   of the basis.
+   The old command line interface of version 1.x is available when you
+   compile with LEGACY=true (using the provided Makefile) or compile with the
+   preprocessor define SOPLEX_LEGACY.
+*/
+//-----------------------------------------------------------------------------
+/**@page LIB How to use SoPlex as a callable library
 
-   Setting the different epsilons to a smaller value like 1e-18 or 1e-20
-   (using \c -zz or \c -zu) might improve the quality of the
-   solution, but may also slow down the program. Setting the epsilons to
-   bigger values may speed up the algorithm, but values greater than 1e-12 
-   are definitely a bad idea.
+   The main interface is given by the class \ref SoPlex, which handles the
+   construction and modification of an LP, the solving process, allows to
+   access and change parameters, and retrive solution information.
 
-   Setting \c -d to smaller values like 1e-7 or 1e-8 will improve the
-   quality of the solution, but it will take longer. Values smaller
-   then 1e-9 are not recommended. The \c -d value should be
-   substantial bigger then the \c -z values.
-
-   If the default settings are too slow, using \c -e eventually together 
-   with \c -p1 might improve the running time.  
+   With version 2.0, the SoPlex class has been updated significantly compared
+   to the 1.x version.  Although this is deprecated, it is still possible to use
+   the old interface class by compiling with LEGACY=true (using the provided
+   Makefile) or defining the preprocessor flag SOPLEX_LEGACY.
 */
 //-----------------------------------------------------------------------------
 /**@page PROG Programming with SoPlex 
@@ -372,25 +255,6 @@
    numerically troublesome LPs and applications that require solutions within
    tight feasibility tolerances.
 
-   Iterative refinement is still under development and deactivated by default.
-   In the following, we explain in detail how to install and use iterative
-   refinement in SoPlex 1.7.  Be aware that the interface may change in future
-   releases.
-
-   @section IR-Installation Installation
-
-   For iterative refinement, SoPlex must be built with support for
-   GMP, the GNU Multiple Precision library (http://www.gmplib.org/).  Since
-   SoPlex uses the C++ interface of GMP, both libgmp and libgmpxx must be
-   available on your system.
-
-   When using SoPlex's Makefile build system, it suffices to make with option
-   GMP=true, see the INSTALL file for details.  If you use a different build
-   system than the provided Makefile, you need to define the preprocessor flag
-   SOPLEX_WITH_GMP and link with libgmp and libgmpxx.
-
-   @section IR-Usage Usage
-
    For a detailed explanation of the iterative refinement algorithm see the ZIB
    technical report 12-19 available online at
 
@@ -402,6 +266,9 @@
    objective function.  The solution to this LP is then used to correct the
    solution and reduce primal and dual violation.  This is repeated until the
    requested feasibility and optimality tolerance is reached.
+
+   For this, SoPlex needs to be compiled with GMP support, see the INSTALL file
+   in the distribution.
 
  */
 //-----------------------------------------------------------------------------
