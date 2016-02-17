scipopt/soplex

commit 6021f1d89612ab06d54026f3d0f4d98ff57f62fa

remove some hard float compares

src/enter.cpp
@@ -1120,7 +1120,7 @@ bool SPxSolver::enter(SPxId& enterId)
    {
       if (spxAbs(leaveVal) < entertol())
       {
-         if (theUBbound[leaveIdx] != theLBbound[leaveIdx] 
+         if (EQ(theUBbound[leaveIdx], theLBbound[leaveIdx])
             && enterStat != Desc::P_FREE && enterStat != Desc::D_FREE) 
             m_numCycle++;
       }
@@ -1228,7 +1228,7 @@ bool SPxSolver::enter(SPxId& enterId)
    }
    /*  No leaving vector could be found that would yield a stable pivot step.
     */
-   else if (leaveVal != -enterMax)
+   else if (NE(leaveVal, -enterMax))
    {
       /* In the ENTER algorithm, when for a selected entering variable we find only
          an instable leaving variable, then the basis change is not conducted.

src/changesoplex.cpp
@@ -757,7 +757,7 @@ void SPxSolver::changeLowerStatus(int i, Real newLower, Real oldLower )
                objChange = (theUCbound[i] * currUpper) - (theLCbound[i] * oldLower);
          }
       }
-      else if (newLower == currUpper)
+      else if (EQ(newLower, currUpper))
       {
          stat = SPxBasis::Desc::P_FIXED;
          if( m_nonbasicValueUpToDate && rep() == COLUMN )
@@ -779,7 +779,7 @@ void SPxSolver::changeLowerStatus(int i, Real newLower, Real oldLower )
       }
       break;
    case SPxBasis::Desc::P_FIXED:
-      if (newLower != currUpper)
+      if (NE(newLower, currUpper))
          stat = SPxBasis::Desc::P_ON_UPPER;
       break;
    case SPxBasis::Desc::D_FREE:
@@ -822,7 +822,7 @@ void SPxSolver::changeLower(int i, const Real& newLower)
 {
    Real oldLower = lower(i);
 
-   if (newLower != oldLower)
+   if (NE(newLower, oldLower))
    {
       // This has to be done before calling changeLowerStatus() because that is calling
       // basis.dualColStatus() which calls lower() and needs the changed value.

src/leave.cpp
@@ -659,7 +659,7 @@ bool SPxSolver::leave(int leaveIdx)
    boundflips = 0;
    Real oldShift = theShift;
    SPxId enterId = theratiotester->selectEnter(enterVal, leaveIdx);
-   if (theShift != oldShift)
+   if (NE(theShift, oldShift))
    {
       MSG_DEBUG( std::cout << "DLEAVE71 trigger recomputation of nonbasic value due to shifts in ratiotest" << std::endl; )
       forceRecompNonbasicValue();
@@ -683,7 +683,7 @@ bool SPxSolver::leave(int leaveIdx)
       change(-1, none, 0);
       objChange = 0.0; // the nonbasicValue is not supposed to be updated in this case
 
-      if (enterVal != leaveMax)
+      if (NE(enterVal, leaveMax))
       {
          MSG_DEBUG( std::cout << "DLEAVE61 rejecting leave A (leaveIdx=" << leaveIdx
                            << ", theCoTest=" << theCoTest[leaveIdx] << ")"

src/spxmainsm.h
@@ -730,8 +730,8 @@ private:
          , m_old_i(lp.nRows()-1)
          , m_obj(lp.spxSense() == SPxLP::MINIMIZE ? lp.obj(_j) : -lp.obj(_j))
          , m_lRhs(slackVal)
-         , m_onLhs(slackVal == lp.lhs(_i))
-         , m_eqCons(lp.lhs(_i) == lp.rhs(_i))
+         , m_onLhs(EQ(slackVal, lp.lhs(_i)))
+         , m_eqCons(EQ(lp.lhs(_i), lp.rhs(_i)))
          , m_row(lp.rowVector(_i))
       {
          assert(m_row[m_j] != 0.0);
@@ -807,7 +807,7 @@ private:
          , m_k(_k)
          , m_i(_i)
          , m_maxSense(lp.spxSense() == SPxLP::MAXIMIZE)
-         , m_jFixed(lp.lower(_j) == lp.upper(_j))
+         , m_jFixed(EQ(lp.lower(_j), lp.upper(_j)))
          , m_jObj(lp.spxSense() == SPxLP::MINIMIZE ? lp.obj(_j) : -lp.obj(_j))
          , m_kObj(lp.spxSense() == SPxLP::MINIMIZE ? lp.obj(_k) : -lp.obj(_k))
          , m_aij(lp.colVector(_j).value(0))

commit 9316ef657537c74b7a10d94db295090a0cb1c6a2

fix wrong comparison introduced with commit 6021f1d

src/enter.cpp
@@ -1142,7 +1142,7 @@ bool SPxSolver::enter(SPxId& enterId)
    {
       if (spxAbs(leaveVal) < entertol())
       {
-         if (EQ(theUBbound[leaveIdx], theLBbound[leaveIdx])
+         if (NE(theUBbound[leaveIdx], theLBbound[leaveIdx])
             && enterStat != Desc::P_FREE && enterStat != Desc::D_FREE) 
             m_numCycle++;
       }