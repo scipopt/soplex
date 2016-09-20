d90dfe5fcb3401b0219df72137096ac925137e41
Matthias Miltenberger <miltenberger@zib.de>
2016-09-20 11:10:49 +0200

    remove unused bool parameter FEASRELAX

--- src/soplex.h
+++ src/soplex.h
@@ -850,14 +850,11 @@ public:
       /// continue iterative refinement with exact basic solution if not optimal?
       RATFACJUMP = 7,
 
-      /// should feasibility be tested with relaxed bounds and sides?
-      FEASRELAX = 8,
-
       /// use bound flipping also for row representation?
-      ROWBOUNDFLIPS = 9,
+      ROWBOUNDFLIPS = 8,
 
       /// number of boolean parameters
-      BOOLPARAM_COUNT = 10
+      BOOLPARAM_COUNT = 9
    } BoolParam;
 
    /// integer parameters
