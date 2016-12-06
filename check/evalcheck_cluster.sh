088e449205b85e6abe21fe510d357b73c02dae14
Stephen J Maher <maher@zib.de>
2016-12-06 08:21:00 +0100

    evaluation output only prints decomp stats if -d flag is provided

--- check/evalcheck_cluster.sh
+++ check/evalcheck_cluster.sh
@@ -17,6 +17,7 @@
 export LANG=C
 
 REMOVE=0
+DECOMP=0
 AWKARGS=""
 FILES=""
 
@@ -29,6 +30,11 @@ do
             REMOVE=1
         else
             AWKARGS="$AWKARGS $i"
+        fi
+
+        if test "$i" = "-d"
+        then
+            DECOMP=1
         fi
     else
         FILES="$FILES $i"
@@ -171,7 +177,11 @@ do
             fi
         fi
 
-        ./evaluation.py $OUTFILE | tee $RESFILE
+        if "$DECOMP" = "1"
+            ./evaluation.py -d $OUTFILE | tee $RESFILE
+        else
+            ./evaluation.py $OUTFILE | tee $RESFILE
+        fi
 
     fi
 done
