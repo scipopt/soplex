4cafdf9cf4c8f92eedfa0b531389d4787750eb0c
Matthias Miltenberger <miltenberger@zib.de>
2017-02-27 09:19:29 +0100

    remove asserts for cases that need to handled properly

--- src/spxbasis.h
+++ src/spxbasis.h
@@ -668,8 +668,6 @@ public:
    /// solves two systems in one call.
    void solve4update(SSVector& x, Vector& y, const SVector& rhsx, SSVector& rhsy)
    {
-      assert(rhsx.size() > 0);
-      assert(rhsy.size() > 0);
       if (!factorized)
          SPxBasis::factorize();
       factor->solve2right4update(x, y, rhsx, rhsy);
@@ -677,8 +675,6 @@ public:
    /// solves two systems in one call using only sparse data structures
    void solve4update(SSVector& x, SSVector& y, const SVector& rhsx, SSVector& rhsy)
    {
-      assert(rhsx.size() > 0);
-      assert(rhsy.size() > 0);
       if (!factorized)
          SPxBasis::factorize();
       factor->solve2right4update(x, y, rhsx, rhsy);
@@ -687,9 +683,6 @@ public:
    void solve4update(SSVector& x, Vector& y, Vector& y2,
                      const SVector& rhsx, SSVector& rhsy, SSVector& rhsy2)
    {
-      assert(rhsx.size() > 0);
-      assert(rhsy.size() > 0);
-      assert(rhsy2.size() > 0);
       if (!factorized) 
          SPxBasis::factorize();
       assert(rhsy.isSetup());
@@ -700,9 +693,6 @@ public:
    void solve4update(SSVector& x, SSVector& y, SSVector& y2,
                      const SVector& rhsx, SSVector& rhsy, SSVector& rhsy2)
    {
-      assert(rhsx.size() > 0);
-      assert(rhsy.size() > 0);
-      assert(rhsy2.size() > 0);
       if (!factorized)
          SPxBasis::factorize();
       assert(rhsy.isSetup());
@@ -745,8 +735,6 @@ public:
    /// solves two systems in one call.
    void coSolve(SSVector& x, Vector& y, const SVector& rhsx, SSVector& rhsy)
    {
-      assert(rhsx.size() > 0);
-      assert(rhsy.size() > 0);
       if (!factorized)
          SPxBasis::factorize();
       factor->solveLeft(x, y, rhsx, rhsy);
@@ -754,8 +742,6 @@ public:
    /// Sparse version of solving two systems in one call.
    void coSolve(SSVector& x, SSVector& y, const SVector& rhsx, SSVector& rhsy)
    {
-      assert(rhsx.size() > 0);
-      assert(rhsy.size() > 0);
       if (!factorized) 
          SPxBasis::factorize();
       factor->solveLeft(x, y, rhsx, rhsy);
@@ -763,9 +749,6 @@ public:
    /// solves three systems in one call. May be improved by using just one pass through the basis.
    void coSolve(SSVector& x, Vector& y, Vector& z, const SVector& rhsx, SSVector& rhsy, SSVector& rhsz)
    {
-      assert(rhsx.size() > 0);
-      assert(rhsy.size() > 0);
-      assert(rhsz.size() > 0);
       if (!factorized)
          SPxBasis::factorize();
       factor->solveLeft(x, y, z, rhsx, rhsy, rhsz);
@@ -773,9 +756,6 @@ public:
    /// Sparse version of solving three systems in one call.
    void coSolve(SSVector& x, SSVector& y, SSVector& z, const SVector& rhsx, SSVector& rhsy, SSVector& rhsz)
    {
-      assert(rhsx.size() > 0);
-      assert(rhsy.size() > 0);
-      assert(rhsz.size() > 0);
       if (!factorized)
          SPxBasis::factorize();
       factor->solveLeft(x, y, z, rhsx, rhsy, rhsz);
