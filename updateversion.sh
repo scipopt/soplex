471f205d53dce7ca3e62f70632e9e4667ca8420e
Matthias Miltenberger <miltenberger@zib.de>
2017-04-07 16:55:58 +0200

    update updateversion script to allow increasing the API version

--- updateversion.sh
+++ updateversion.sh
@@ -2,26 +2,25 @@
 #
 # Script to update all version numbers in SoPlex
 #
+# usage: updateversion.sh [<major> <minor> <patch>]"
+#
 
-if [ $# -eq 4 ]
+if [[ $# -eq 0 ]]
 then
-    sub=".$4"
-    subzero="        $4"
-elif [ $# -eq 3 ]
+    echo "updated API version:"
+    perl -i -pe 's/#define SOPLEX_APIVERSION \K(\d+)/$1 + 1/e' src/spxdefines.h
+    grep SOPLEX_APIVERSION src/spxdefines.h
+elif [[ $# -eq 3 ]]
 then
-    sub=""
-    subzero="        0"
+    echo "new version:"
+    sed -i "s/^VERSION.*/VERSION		:=	$1.$2.$3/" Makefile
+    sed -i "s/^VERSION.*/VERSION		=	$1.$2.$3/" Makefile.nmake
+    sed -i "s/^ \* @version.*/ \* @version  $1.$2.$3/" doc/xternal.cpp
+    sed -i "s/^VERSION=.*/VERSION=\"$1.$2.$3\"/" makedist.sh
+    sed -i "s/^#define SOPLEX_VERSION.*/#define SOPLEX_VERSION         $1$2$3/" src/spxdefines.h
+    grep SOPLEX_VERSION src/spxdefines.h
 else
-    echo "usage: $0 <major_1> <major_2> <major_3> [<minor>]"
-    echo "update all version numbers to the specified one"
-    echo "example dev:     $0 3 0 1 2"
-    echo "example release: $0 3 0 2"
+    echo "usage: $0 [<major> <minor> <patch>]"
+    echo "update all version numbers to the specified one or only increase API version"
     exit 1;
 fi
-
-sed -i "s/^VERSION.*/VERSION		:=	$1.$2.$3$sub/" Makefile
-sed -i "s/^VERSION.*/VERSION		=	$1.$2.$3$sub/" Makefile.nmake
-sed -i "s/^ \* @version.*/ \* @version  $1.$2.$3$sub/" doc/xternal.cpp
-sed -i "s/^VERSION=.*/VERSION=\"$1.$2.$3$sub\"/" makedist.sh
-sed -i "s/^#define SOPLEX_VERSION.*/#define SOPLEX_VERSION         $1$2$3/" src/spxdefines.h
-sed -i "s/^#define SOPLEX_SUBVERSION.*/#define SOPLEX_SUBVERSION$subzero/" src/spxdefines.h
