b6ef617d1e28b3b91b3686204fc73da61c8d27a3
Matthias Miltenberger <miltenberger@zib.de>
2017-04-26 19:03:32 +0200

    correct version number to 3.0.0.1 and adapt script

--- updateversion.sh
+++ updateversion.sh
@@ -7,18 +7,30 @@
 
 if [[ $# -eq 0 ]]
 then
+    perl -i -pe 's/#define SOPLEX_APIVERSION        \K(\d+)/$1 + 1/e' src/spxdefines.h
     echo "updated API version:"
-    perl -i -pe 's/#define SOPLEX_APIVERSION \K(\d+)/$1 + 1/e' src/spxdefines.h
     grep SOPLEX_APIVERSION src/spxdefines.h
+
+elif [[ $# -eq 4 ]]
+then
+    sed -i "s/^VERSION.*/VERSION		:=	$1.$2.$3.$4/" Makefile
+    sed -i "s/^VERSION.*/VERSION		=	$1.$2.$3.$4/" Makefile.nmake
+    sed -i "s/^ \* @version.*/ \* @version  $1.$2.$3.$4/" doc/xternal.cpp
+    sed -i "s/^VERSION=.*/VERSION=\"$1.$2.$3.$4\"/" makedist.sh
+    sed -i "s/^#define SOPLEX_VERSION.*/#define SOPLEX_VERSION         $1$2$3/" src/spxdefines.h
+    sed -i "s/^#define SOPLEX_SUBVERSION.*/#define SOPLEX_SUBVERSION        $4/" src/spxdefines.h
+    echo "new version:"
+    grep -e SOPLEX_VERSION -e SOPLEX_SUBVERSION -e SOPLEX_APIVERSION src/spxdefines.h
 elif [[ $# -eq 3 ]]
 then
-    echo "new version:"
     sed -i "s/^VERSION.*/VERSION		:=	$1.$2.$3/" Makefile
     sed -i "s/^VERSION.*/VERSION		=	$1.$2.$3/" Makefile.nmake
     sed -i "s/^ \* @version.*/ \* @version  $1.$2.$3/" doc/xternal.cpp
     sed -i "s/^VERSION=.*/VERSION=\"$1.$2.$3\"/" makedist.sh
     sed -i "s/^#define SOPLEX_VERSION.*/#define SOPLEX_VERSION         $1$2$3/" src/spxdefines.h
-    grep SOPLEX_VERSION src/spxdefines.h
+    sed -i "s/^#define SOPLEX_SUBVERSION.*/#define SOPLEX_SUBVERSION        0/" src/spxdefines.h
+    echo "new version:"
+    grep -e SOPLEX_VERSION -e SOPLEX_SUBVERSION -e SOPLEX_APIVERSION src/spxdefines.h
 else
     echo "usage: $0 [<major> <minor> <patch>]"
     echo "update all version numbers to the specified one or only increase API version"
