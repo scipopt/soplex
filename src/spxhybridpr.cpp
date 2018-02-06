58eb571223ef20c25982ccc437b47ce458568e27
bzfmulac <mulackal@zib.de>
2018-02-06 17:44:48 +0100

    Fixed intendation in spxhybridpr.cpp

--- src/spxhybridpr.cpp
+++ src/spxhybridpr.cpp
@@ -22,130 +22,130 @@
 namespace soplex
 {
 
-    template <class R>
-bool SPxHybridPR<R>::isConsistent() const
-{
+  template <class R>
+  bool SPxHybridPR<R>::isConsistent() const
+  {
 #ifdef ENABLE_CONSISTENCY_CHECKS
-   if (thesolver != 0 &&
-      (thesolver != steep.this->solver() ||
+    if (thesolver != 0 &&
+        (thesolver != steep.this->solver() ||
          thesolver != devex.this->solver() ||
          thesolver != parmult.this->solver()))
       return MSGinconsistent("SPxHybridPR");
 
-   return steep.isConsistent()
-          && devex.isConsistent()
-          && parmult.isConsistent();
+    return steep.isConsistent()
+      && devex.isConsistent()
+      && parmult.isConsistent();
 #else
-   return true;
+    return true;
 #endif
-}
+  }
 
   template <class R>
   void SPxHybridPR<R>::load(SPxSolver<R>* p_solver)
-{
-   steep.load(p_solver);
-   devex.load(p_solver);
-   parmult.load(p_solver);
-   this->thesolver = p_solver;
-   setType(p_solver->type());
-}
+  {
+    steep.load(p_solver);
+    devex.load(p_solver);
+    parmult.load(p_solver);
+    this->thesolver = p_solver;
+    setType(p_solver->type());
+  }
 
   template <class R>
-void SPxHybridPR<R>::clear()
-{
-   steep.clear();
-   devex.clear();
-   parmult.clear();
-   this->thesolver = 0;
-}
+  void SPxHybridPR<R>::clear()
+  {
+    steep.clear();
+    devex.clear();
+    parmult.clear();
+    this->thesolver = 0;
+  }
 
   template <class R>
-void SPxHybridPR<R>::setEpsilon(Real eps)
-{
-   steep.setEpsilon(eps);
-   devex.setEpsilon(eps);
-   parmult.setEpsilon(eps);
-}
+  void SPxHybridPR<R>::setEpsilon(Real eps)
+  {
+    steep.setEpsilon(eps);
+    devex.setEpsilon(eps);
+    parmult.setEpsilon(eps);
+  }
 
   template <class R>
-void SPxHybridPR<R>::setType(typename SPxSolver<R>::Type tp)
-{
-   if (tp == SPxSolver<R>::LEAVE)
-   {
-      thepricer = &steep;
-      this->thesolver->setPricing(SPxSolver<R>::FULL);
-   }
-   else
-   {
-      if (this->thesolver->dim() > hybridFactor * this->thesolver->coDim())
+  void SPxHybridPR<R>::setType(typename SPxSolver<R>::Type tp)
+  {
+    if (tp == SPxSolver<R>::LEAVE)
       {
-         /**@todo I changed from devex to steepest edge pricing here 
-          *       because of numerical difficulties, this should be 
-          *       investigated.
-          */
-         // thepricer = &devex;
-         thepricer = &steep;
-         this->thesolver->setPricing(SPxSolver<R>::FULL);
+        thepricer = &steep;
+        this->thesolver->setPricing(SPxSolver<R>::FULL);
       }
-      else
+    else
       {
-         thepricer = &parmult;
-         this->thesolver->setPricing(SPxSolver<R>::PARTIAL);
+        if (this->thesolver->dim() > hybridFactor * this->thesolver->coDim())
+          {
+            /**@todo I changed from devex to steepest edge pricing here 
+             *       because of numerical difficulties, this should be 
+             *       investigated.
+             */
+            // thepricer = &devex;
+            thepricer = &steep;
+            this->thesolver->setPricing(SPxSolver<R>::FULL);
+          }
+        else
+          {
+            thepricer = &parmult;
+            this->thesolver->setPricing(SPxSolver<R>::PARTIAL);
+          }
       }
-   }
    
-   MSG_INFO1( (*this->thesolver->spxout), (*this->thesolver->spxout) << "IPRHYB01 switching to "
-                        << thepricer->getName() << std::endl; )
+    MSG_INFO1( (*this->thesolver->spxout), (*this->thesolver->spxout) << "IPRHYB01 switching to "
+               << thepricer->getName() << std::endl; )
 
-   thepricer->setType(tp);
-}
-
-  template <class R>
-void SPxHybridPR<R>::setRep(typename SPxSolver<R>::Representation rep)
-{
-   steep.setRep(rep);
-   devex.setRep(rep);
-   parmult.setRep(rep);
-}
+      thepricer->setType(tp);
+  }
 
   template <class R>
-int SPxHybridPR<R>::selectLeave()
-{
-   return thepricer->selectLeave();
-}
+  void SPxHybridPR<R>::setRep(typename SPxSolver<R>::Representation rep)
+  {
+    steep.setRep(rep);
+    devex.setRep(rep);
+    parmult.setRep(rep);
+  }
 
   template <class R>
-void SPxHybridPR<R>::left4(int n, SPxId id)
-{
-   thepricer->left4(n, id);
-}
+  int SPxHybridPR<R>::selectLeave()
+  {
+    return thepricer->selectLeave();
+  }
 
   template <class R>
-SPxId SPxHybridPR<R>::selectEnter()
-{
-   return thepricer->selectEnter();
-}
+  void SPxHybridPR<R>::left4(int n, SPxId id)
+  {
+    thepricer->left4(n, id);
+  }
 
   template <class R>
-void SPxHybridPR<R>::entered4(SPxId id, int n)
-{
-   thepricer->entered4(id, n);
-}
+  SPxId SPxHybridPR<R>::selectEnter()
+  {
+    return thepricer->selectEnter();
+  }
 
   template <class R>
-void SPxHybridPR<R>::addedVecs (int n)
-{
-   steep.addedVecs(n);
-   devex.addedVecs(n);
-   parmult.addedVecs(n);
-}
+  void SPxHybridPR<R>::entered4(SPxId id, int n)
+  {
+    thepricer->entered4(id, n);
+  }
 
   template <class R>
-void SPxHybridPR<R>::addedCoVecs(int n)
-{
-   steep.addedCoVecs(n);
-   devex.addedCoVecs(n);
-   parmult.addedCoVecs(n);
-}
+  void SPxHybridPR<R>::addedVecs (int n)
+  {
+    steep.addedVecs(n);
+    devex.addedVecs(n);
+    parmult.addedVecs(n);
+  }
+
+  template <class R>
+  void SPxHybridPR<R>::addedCoVecs(int n)
+  {
+    steep.addedCoVecs(n);
+    devex.addedCoVecs(n);
+    parmult.addedCoVecs(n);
+  }
 
 } // namespace soplex
