c72c76f5a28a018477455f2eb524b96004d5fddf
bzfmulac <mulackal@zib.de>
2018-02-13 17:48:48 +0100

    Changed intendation of validation.cpp and .h

--- src/validation.cpp
+++ src/validation.cpp
@@ -21,124 +21,124 @@
 
 namespace soplex {
 
-/// updates the external solution used for validation
+  /// updates the external solution used for validation
   template <class R>
   bool Validation<R>::updateExternalSolution(char* solution)
-{
-   validate = true;
-   validatesolution = solution;
+  {
+    validate = true;
+    validatesolution = solution;
 
-   if( strncmp(solution, "+infinity", 9 ) == 0 )
+    if( strncmp(solution, "+infinity", 9 ) == 0 )
       return true;
-   else if ( strncmp(solution, "-infinity", 9) == 0 )
+    else if ( strncmp(solution, "-infinity", 9) == 0 )
       return true;
-   else
-   {
-      char* tailptr;
-      strtod(solution, &tailptr);
-      if (*tailptr) {
-         //conversion failed because the input wasn't a number
-         return false;
+    else
+      {
+        char* tailptr;
+        strtod(solution, &tailptr);
+        if (*tailptr) {
+          //conversion failed because the input wasn't a number
+          return false;
+        }
       }
-   }
-   return true;
-}
+    return true;
+  }
 
 
 
-/// updates the tolerance used for validation
+  /// updates the tolerance used for validation
   template <class R>
   bool Validation<R>::updateValidationTolerance(char* tolerance)
-{
-   char* tailptr;
-   validatetolerance = strtod(tolerance, &tailptr);
-   if (*tailptr) {
+  {
+    char* tailptr;
+    validatetolerance = strtod(tolerance, &tailptr);
+    if (*tailptr) {
       //conversion failed because the input wasn't a number
       return false;
-   }
-   return true;
-}
+    }
+    return true;
+  }
 
 
 
-/// validates the soplex solution using the external solution
+  /// validates the soplex solution using the external solution
   template <class R>
-void Validation<R>::validateSolveReal(SoPlex<R>& soplex)
-{
+  void Validation<R>::validateSolveReal(SoPlex<R>& soplex)
+  {
 #ifndef SOPLEX_LEGACY
-   bool passedValidation = true;
-   std::string reason = "";
-   Real objViolation = 0.0;
-   Real maxBoundViolation = 0.0;
-   Real maxRowViolation = 0.0;
-   Real maxRedCostViolation = 0.0;
-   Real maxDualViolation = 0.0;
-   Real sumBoundViolation = 0.0;
-   Real sumRowViolation = 0.0;
-   Real sumRedCostViolation = 0.0;
-   Real sumDualViolation = 0.0;
-   Real sol;
+    bool passedValidation = true;
+    std::string reason = "";
+    Real objViolation = 0.0;
+    Real maxBoundViolation = 0.0;
+    Real maxRowViolation = 0.0;
+    Real maxRedCostViolation = 0.0;
+    Real maxDualViolation = 0.0;
+    Real sumBoundViolation = 0.0;
+    Real sumRowViolation = 0.0;
+    Real sumRedCostViolation = 0.0;
+    Real sumDualViolation = 0.0;
+    Real sol;
 
-   std::ostream& os = soplex.spxout.getStream(SPxOut::INFO1);
+    std::ostream& os = soplex.spxout.getStream(SPxOut::INFO1);
 
-   if( strncmp(validatesolution, "+infinity", 9 ) == 0 )
+    if( strncmp(validatesolution, "+infinity", 9 ) == 0 )
       sol =  soplex.realParam(SoPlex<R>::INFTY);
-   else if ( strncmp(validatesolution, "-infinity", 9) == 0 )
+    else if ( strncmp(validatesolution, "-infinity", 9) == 0 )
       sol =  -soplex.realParam(SoPlex<R>::INFTY);
-   else
-   {
-      sol = atof(validatesolution);
-   }
+    else
+      {
+        sol = atof(validatesolution);
+      }
 
-   objViolation = spxAbs(sol - soplex.objValueReal());
-   if( ! EQ(objViolation, 0.0, validatetolerance) )
-   {
-      passedValidation = false;
-      reason += "Objective Violation; ";
-   }
-   if( SPxSolver<R>::OPTIMAL == soplex.status() )
-   {
-      soplex.getBoundViolationReal(maxBoundViolation, sumBoundViolation);
-      soplex.getRowViolationReal(maxRowViolation, sumRowViolation);
-      soplex.getRedCostViolationReal(maxRedCostViolation, sumRedCostViolation);
-      soplex.getDualViolationReal(maxDualViolation, sumDualViolation);
-      if( ! LE(maxBoundViolation, validatetolerance) )
+    objViolation = spxAbs(sol - soplex.objValueReal());
+    if( ! EQ(objViolation, 0.0, validatetolerance) )
       {
-         passedValidation = false;
-         reason += "Bound Violation; ";
+        passedValidation = false;
+        reason += "Objective Violation; ";
       }
-      if( ! LE(maxRowViolation, validatetolerance) )
+    if( SPxSolver<R>::OPTIMAL == soplex.status() )
       {
-         passedValidation = false;
-         reason += "Row Violation; ";
+        soplex.getBoundViolationReal(maxBoundViolation, sumBoundViolation);
+        soplex.getRowViolationReal(maxRowViolation, sumRowViolation);
+        soplex.getRedCostViolationReal(maxRedCostViolation, sumRedCostViolation);
+        soplex.getDualViolationReal(maxDualViolation, sumDualViolation);
+        if( ! LE(maxBoundViolation, validatetolerance) )
+          {
+            passedValidation = false;
+            reason += "Bound Violation; ";
+          }
+        if( ! LE(maxRowViolation, validatetolerance) )
+          {
+            passedValidation = false;
+            reason += "Row Violation; ";
+          }
+        if( ! LE(maxRedCostViolation, validatetolerance) )
+          {
+            passedValidation = false;
+            reason += "Reduced Cost Violation; ";
+          }
+        if( ! LE(maxDualViolation, validatetolerance) )
+          {
+            passedValidation = false;
+            reason += "Dual Violation; ";
+          }
       }
-      if( ! LE(maxRedCostViolation, validatetolerance) )
-      {
-         passedValidation = false;
-         reason += "Reduced Cost Violation; ";
-      }
-      if( ! LE(maxDualViolation, validatetolerance) )
-      {
-         passedValidation = false;
-         reason += "Dual Violation; ";
-      }
-   }
 
-   os << "\n";
-   os << "Validation          :";
-   if(passedValidation)
+    os << "\n";
+    os << "Validation          :";
+    if(passedValidation)
       os << " Success\n";
-   else
-   {
-      reason[reason.length()-2] = ']';
-      os << " Fail [" + reason + "\n";
-   }
-   os << "   Objective        : " << std::scientific << std::setprecision(8) << objViolation << std::fixed << "\n";
-   os << "   Bound            : " << std::scientific << std::setprecision(8) << maxBoundViolation << std::fixed << "\n";
-   os << "   Row              : " << std::scientific << std::setprecision(8) << maxRowViolation << std::fixed << "\n";
-   os << "   Reduced Cost     : " << std::scientific << std::setprecision(8) << maxRedCostViolation << std::fixed << "\n";
-   os << "   Dual             : " << std::scientific << std::setprecision(8) << maxDualViolation << std::fixed << "\n";
+    else
+      {
+        reason[reason.length()-2] = ']';
+        os << " Fail [" + reason + "\n";
+      }
+    os << "   Objective        : " << std::scientific << std::setprecision(8) << objViolation << std::fixed << "\n";
+    os << "   Bound            : " << std::scientific << std::setprecision(8) << maxBoundViolation << std::fixed << "\n";
+    os << "   Row              : " << std::scientific << std::setprecision(8) << maxRowViolation << std::fixed << "\n";
+    os << "   Reduced Cost     : " << std::scientific << std::setprecision(8) << maxRedCostViolation << std::fixed << "\n";
+    os << "   Dual             : " << std::scientific << std::setprecision(8) << maxDualViolation << std::fixed << "\n";
 #endif
-}
+  }
 
 } /* namespace soplex */
