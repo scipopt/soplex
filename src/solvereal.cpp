e649620619c3edfbe7f2c8f5121b17dfb7aeab0f
Matthias Miltenberger <miltenberger@zib.de>
2018-04-09 16:37:41 +0200

    nicer indentation and a better info message

--- src/solvereal.cpp
+++ src/solvereal.cpp
@@ -397,7 +397,7 @@ namespace soplex
                                    << ", row violation: " << rowviol
                                    << ", dual violation: " << dualviol
                                    << ", redcost violation: " << redcostviol << std::endl; )
-         MSG_INFO1( spxout, spxout << " --- detected violations in original problem space -- solve again without presolving" << std::endl; )
+         MSG_INFO1( spxout, spxout << " --- detected violations in original problem space -- solve again without presolving/scaling" << std::endl; )
 
          if( _isRealLPScaled )
          {
