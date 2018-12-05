38f55a8ea925323fe48aa479e18b7a3c395220bc
Matthias Miltenberger <miltenberger@zib.de>
2018-12-05 23:18:24 +0100

    use correct precompiler check

--- src/soplex/stablesum.h
+++ src/soplex/stablesum.h
@@ -57,7 +57,7 @@ class StableSum<double>
 
    void operator+=( double input )
    {
-#if _MSC_VER || __INTEL_COMPILER
+#ifdef _MSC_VER || __INTEL_COMPILER
       #pragma float_control( precise, on )
 #endif
 
