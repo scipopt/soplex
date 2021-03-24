6ca748876d32f3b727b2583c9627e97ca4758c7e
Antonia Chmiela <chmiela@zib.de>
2021-03-24 16:56:37 +0100

    add rational test -- still needs fixing

--- tests/c_interface/main.c
+++ tests/c_interface/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <soplex_interface.h>
 #include <assert.h>
+#include <string.h>
 
 void test_real(void)
 {
@@ -66,11 +67,77 @@ void test_real(void)
 	SoPlex_free(soplex2);
 }
 
+void test_rational(void)
+{
+   /* create LP via rows */
+
+   void *soplex = SoPlex_create();
+   int infty = 1000000;
+   int rownums[] = {-1, 1};
+   int rowdenoms[] = {1, 1};
+   int objnums[] = {1, 1};
+   int objdenoms[] = {1, 1};
+   double primal[] = {0.0,0.0};
+
+   /* use rational solver */
+   SoPlex_setRational(soplex);
+
+   /* minimize */
+   SoPlex_setIntParam(soplex, 0, -1);
+
+   /* add row and set objective function */
+   SoPlex_addRowRational(soplex, rownums, rowdenoms, 2, 2, 1, 5, infty, 1);
+   SoPlex_changeObjRational(soplex, objnums, objdenoms, 2);
+
+   /* optimize and check rational solution and objective value */
+   int result = SoPlex_optimize(soplex);
+   assert(result == 1);
+   assert(strcmp(SoPlex_getPrimalRationalString(soplex, 2), "0 1/5 ") == 0);
+   assert(strcmp(SoPlex_objValueRationalString(soplex), "1/5") == 0);
+
+   SoPlex_free(soplex);
+
+   /* create LP via columns */
+   void *soplex2 = SoPlex_create();
+   int colnums1[] = {2};
+   int coldenoms1[] = {1};
+   int colnums2[] = {1};
+   int coldenoms2[] = {1};
+   int lhsnums[] = {1000};
+   int lhsdenoms[] = {1};
+
+   /* use rational solver */
+   SoPlex_setRational(soplex2);
+
+   /* minimize */
+   SoPlex_setIntParam(soplex2, 0, -1);
+
+   /* add cols */
+   SoPlex_addColRational(soplex2, colnums1, coldenoms1, 1, 1, 1, 5, 0, 1, infty, 1);
+   //SoPlex_addColRational(soplex2, colnums2, coldenoms2, 1, 1, 1, 1, -infty, 1, infty, 1);
+
+   //SoPlex_addColReal(soplex, colentries1, 1, 1, 1.0, 0.0, infty);
+   //SoPlex_addColReal(soplex, colentries2, 1, 1, 1.0, -infty, infty);
+   /* add bounds to constraint */
+   SoPlex_changeLhsRational(soplex2, lhsnums, lhsdenoms, 1);
+
+   /* optimize and check rational solution and objective value */
+   result = SoPlex_optimize(soplex2);
+   assert(result == 1);
+   printf("%s \n", SoPlex_getPrimalRationalString(soplex2, 1));
+   //assert(strcmp(SoPlex_getPrimalRationalString(soplex2, 2), "0 1/5 ") == 0);
+   //assert(strcmp(SoPlex_objValueRationalString(soplex2), "1/25") == 0);
+
+   SoPlex_free(soplex2);
+
+}
+
 int main(void)
 {
-   printf("testing real... \n");
-   test_real();
+   //printf("testing real... \n");
+   //test_real();
 
    printf("\n");
    printf("testing rational... \n");
+   test_rational();
 }
