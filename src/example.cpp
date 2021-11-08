fd276631006c0d47017132d790e85cb5d4be2868
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-11-08 22:02:02 +0100

    astyle formatting

--- src/example.cpp
+++ src/example.cpp
@@ -27,118 +27,118 @@ using namespace soplex;
 
 void test_real()
 {
-   SoPlex mysoplex;
+    SoPlex mysoplex;
 
-   /* set the objective sense */
-   mysoplex.setIntParam(SoPlex::OBJSENSE, SoPlex::OBJSENSE_MINIMIZE);
+    /* set the objective sense */
+    mysoplex.setIntParam(SoPlex::OBJSENSE, SoPlex::OBJSENSE_MINIMIZE);
 
-   /* we first add variables */
-   DSVector dummycol(0);
-   mysoplex.addColReal(LPCol(3.0, dummycol, infinity, 1.0));
-   mysoplex.addColReal(LPCol(2.0, dummycol, infinity, 1.0));
+    /* we first add variables */
+    DSVector dummycol(0);
+    mysoplex.addColReal(LPCol(3.0, dummycol, infinity, 1.0));
+    mysoplex.addColReal(LPCol(2.0, dummycol, infinity, 1.0));
 
-   /* then constraints one by one */
-   DSVector row1(2);
-   row1.add(0, 0.2);
-   row1.add(1, 1.0);
-   mysoplex.addRowReal(LPRow(2.0, row1, infinity));
+    /* then constraints one by one */
+    DSVector row1(2);
+    row1.add(0, 0.2);
+    row1.add(1, 1.0);
+    mysoplex.addRowReal(LPRow(2.0, row1, infinity));
 
-   /* NOTE: alternatively, we could have added the matrix nonzeros in dummycol already; nonexisting rows are then
-    * created automatically. */
+    /* NOTE: alternatively, we could have added the matrix nonzeros in dummycol already; nonexisting rows are then
+     * created automatically. */
 
-   /* write LP in .lp format */
-   mysoplex.writeFileReal("dump_real.lp", NULL, NULL, NULL);
+    /* write LP in .lp format */
+    mysoplex.writeFileReal("dump_real.lp", NULL, NULL, NULL);
 
-   /* solve LP */
-   SPxSolver::Status stat;
-   DVector prim(2);
-   DVector dual(1);
-   stat = mysoplex.optimize();
+    /* solve LP */
+    SPxSolver::Status stat;
+    DVector prim(2);
+    DVector dual(1);
+    stat = mysoplex.optimize();
 
-   /* get solution */
-   if(stat == SPxSolver::OPTIMAL)
-   {
-      mysoplex.getPrimal(prim);
-      mysoplex.getDual(dual);
-      std::cout << "LP solved to optimality.\n";
-      std::cout << "Objective value is " << mysoplex.objValueReal() << ".\n";
-      std::cout << "Primal solution is [" << prim[0] << ", " << prim[1] << "].\n";
-      std::cout << "Dual solution is [" << dual[0] << "].\n";
-   }
-   else
-   {
-      std::cout << "Error: SoPlex returned with status " << stat << ".\n";
-   }
+    /* get solution */
+    if(stat == SPxSolver::OPTIMAL)
+    {
+        mysoplex.getPrimal(prim);
+        mysoplex.getDual(dual);
+        std::cout << "LP solved to optimality.\n";
+        std::cout << "Objective value is " << mysoplex.objValueReal() << ".\n";
+        std::cout << "Primal solution is [" << prim[0] << ", " << prim[1] << "].\n";
+        std::cout << "Dual solution is [" << dual[0] << "].\n";
+    }
+    else
+    {
+        std::cout << "Error: SoPlex returned with status " << stat << ".\n";
+    }
 }
 
 
 void test_rational()
 {
-   SoPlex mysoplex;
+    SoPlex mysoplex;
 
-   /* set parameters for exact solving */
-   mysoplex.setIntParam(SoPlex::READMODE, SoPlex::READMODE_RATIONAL);
-   mysoplex.setIntParam(SoPlex::SOLVEMODE, SoPlex::SOLVEMODE_RATIONAL);
-   mysoplex.setIntParam(SoPlex::CHECKMODE, SoPlex::CHECKMODE_RATIONAL);
-   mysoplex.setIntParam(SoPlex::SYNCMODE, SoPlex::SYNCMODE_AUTO);
-   mysoplex.setRealParam(SoPlex::FEASTOL, 0.0);
-   mysoplex.setRealParam(SoPlex::OPTTOL, 0.0);
+    /* set parameters for exact solving */
+    mysoplex.setIntParam(SoPlex::READMODE, SoPlex::READMODE_RATIONAL);
+    mysoplex.setIntParam(SoPlex::SOLVEMODE, SoPlex::SOLVEMODE_RATIONAL);
+    mysoplex.setIntParam(SoPlex::CHECKMODE, SoPlex::CHECKMODE_RATIONAL);
+    mysoplex.setIntParam(SoPlex::SYNCMODE, SoPlex::SYNCMODE_AUTO);
+    mysoplex.setRealParam(SoPlex::FEASTOL, 0.0);
+    mysoplex.setRealParam(SoPlex::OPTTOL, 0.0);
 
-   /* set the objective sense */
-   mysoplex.setIntParam(SoPlex::OBJSENSE, SoPlex::OBJSENSE_MINIMIZE);
+    /* set the objective sense */
+    mysoplex.setIntParam(SoPlex::OBJSENSE, SoPlex::OBJSENSE_MINIMIZE);
 
-   /* we first add variables (the integer data is converted to type Rational) */
-   DSVectorRational dummycol(0);
-   mysoplex.addColRational(LPColRational(3, dummycol, infinity, 1));
-   mysoplex.addColRational(LPColRational(2, dummycol, infinity, 1));
+    /* we first add variables (the integer data is converted to type Rational) */
+    DSVectorRational dummycol(0);
+    mysoplex.addColRational(LPColRational(3, dummycol, infinity, 1));
+    mysoplex.addColRational(LPColRational(2, dummycol, infinity, 1));
 
-   /* then constraints one by one (here we show how Rationals can be used directly) */
-   DSVectorRational row1(2);
-   Rational r;
-   r = 1;
-   r /= 5;
-   row1.add(0, r);
-   r = 1;
-   row1.add(1, r);
-   r = 2;
-   mysoplex.addRowRational(LPRowRational(r, row1, infinity));
+    /* then constraints one by one (here we show how Rationals can be used directly) */
+    DSVectorRational row1(2);
+    Rational r;
+    r = 1;
+    r /= 5;
+    row1.add(0, r);
+    r = 1;
+    row1.add(1, r);
+    r = 2;
+    mysoplex.addRowRational(LPRowRational(r, row1, infinity));
 
-   /* NOTE: alternatively, we could have added the matrix nonzeros in dummycol already; nonexisting rows are then
-    * automatically created. */
+    /* NOTE: alternatively, we could have added the matrix nonzeros in dummycol already; nonexisting rows are then
+     * automatically created. */
 
-   /* write LP in .lp format */
-   mysoplex.writeFileRational("dump_rational.lp", NULL, NULL, NULL);
+    /* write LP in .lp format */
+    mysoplex.writeFileRational("dump_rational.lp", NULL, NULL, NULL);
 
-   /* solve LP */
-   SPxSolver::Status stat;
-   DVectorRational prim(2);
-   DVectorRational dual(1);
-   stat = mysoplex.optimize();
+    /* solve LP */
+    SPxSolver::Status stat;
+    DVectorRational prim(2);
+    DVectorRational dual(1);
+    stat = mysoplex.optimize();
 
-   /* get solution */
-   if(stat == SPxSolver::OPTIMAL)
-   {
-      mysoplex.getPrimalRational(prim);
-      mysoplex.getDualRational(dual);
-      std::cout << "LP solved to optimality.\n";
-      std::cout << "Objective value is " << mysoplex.objValueRational() << ".\n";
-      std::cout << "Primal solution is [" << prim[0] << ", " << prim[1] << "].\n";
-      std::cout << "Dual solution is [" << dual[0] << "].\n";
-   }
-   else
-   {
-      std::cout << "Error: SoPlex returned with status " << stat << ".\n";
-   }
+    /* get solution */
+    if(stat == SPxSolver::OPTIMAL)
+    {
+        mysoplex.getPrimalRational(prim);
+        mysoplex.getDualRational(dual);
+        std::cout << "LP solved to optimality.\n";
+        std::cout << "Objective value is " << mysoplex.objValueRational() << ".\n";
+        std::cout << "Primal solution is [" << prim[0] << ", " << prim[1] << "].\n";
+        std::cout << "Dual solution is [" << dual[0] << "].\n";
+    }
+    else
+    {
+        std::cout << "Error: SoPlex returned with status " << stat << ".\n";
+    }
 }
 
 
 int main()
 {
-   std::cout << "Testing SoPlex as floating-point LP solver:\n\n";
-   test_real();
+    std::cout << "Testing SoPlex as floating-point LP solver:\n\n";
+    test_real();
 
-   std::cout << "\nTesting SoPlex as exact rational LP solver:\n\n";
-   test_rational();
+    std::cout << "\nTesting SoPlex as exact rational LP solver:\n\n";
+    test_rational();
 
-   return 0;
+    return 0;
 }
