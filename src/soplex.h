9ffb48227f712bb946160f6683dab7953bd6ff87
Alexander Hoen <hoen@zib.de>
2022-01-11 10:49:10 +0100

    correct presolving settings for presolvers

--- src/soplex.h
+++ src/soplex.h
@@ -1156,13 +1156,13 @@ public:
       /// disabling presolving
       SIMPLIFIER_OFF = 0,
 
-      /// using internal presoling methods
+      /// using internal presolving methods
       SIMPLIFIER_INTERNAL = 3,
 
       /// using the presolve lib papilo
       SIMPLIFIER_PAPILO = 2,
 
-      /// @deprecated: only to provide backwards compatibility (use INTERNAL instead)
+      /// use the default presolver (currently internal)
       SIMPLIFIER_AUTO = 1
    };
 
