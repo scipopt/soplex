30a667e104bb0d8a5a059592a8e43ba14f822b39
Alexander Hoen <hoen@zib.de>
2024-04-09 09:11:46 +0200

    upgrade fmt 6 to 7

--- src/soplex/fmt.hpp
+++ src/soplex/fmt.hpp
@@ -47,8 +47,14 @@
 #define SOPLEX_DEFINED_NOGDI
 #endif
 
+/* to provide backwards compatibility use fmt of PaPILO <= 2.3 due to breaking changes in fmt 7 */
+#if !defined(SOPLEX_WITH_PAPILO) || PAPILO_VERSION_MAJOR > 2 || (PAPILO_VERSION_MAJOR == 2 && PAPILO_VERSION_MINOR > 3)
 #include "soplex/external/fmt/format.h"
 #include "soplex/external/fmt/ostream.h"
+#else
+#include "papilo/external/fmt/format.h"
+#include "papilo/external/fmt/ostream.h"
+#endif
 
 #ifdef SOPLEX_DEFINED_NOGDI
 #undef NOGDI
