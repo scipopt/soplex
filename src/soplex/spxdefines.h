49251ff8607e42836c9c385fac2f7d808a828714
Leon Eifler <eifler@zib.de>
2024-06-12 14:33:44 +0200

    increase APIVERSION

--- src/soplex/spxdefines.h
+++ src/soplex/spxdefines.h
@@ -93,7 +93,7 @@ bool EQ(int a, int b);
 
 #define SOPLEX_VERSION         701
 #define SOPLEX_SUBVERSION        0
-#define SOPLEX_APIVERSION       14
+#define SOPLEX_APIVERSION       15
 #define SOPLEX_COPYRIGHT       "Copyright (c) 1996-2024 Zuse Institute Berlin (ZIB)"
 
 /*-----------------------------------------------------------------------------
